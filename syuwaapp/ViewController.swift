3d701ed33a2c534b5a9e7e256b36541b08093a01
Hinako Nakai <hinako.0113n@gmail.com>
2021-02-07 12:00:33 +0900

    init

---
+++ syuwaapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  syuwaapp
+//
+//  Created by 中井日向子 on 2021/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ syuwaapp/characterListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  characterListViewController.swift
+//  syuwaapp
+//
+//  Created by 中井日向子 on 2021/01/24.
+//
+
+import UIKit
+
+class characterListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
