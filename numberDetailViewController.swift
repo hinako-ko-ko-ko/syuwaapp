edf086da85577eb7a3b62fce897816dc86c70e0d
Hinako Nakai <hinako.0113n@gmail.com>
2021-03-26 21:32:30 +0900

    3月26日 手話アプリです 今は五十音まで出来て、数詞に取り組んでいます 手話の手はフリー素材を引用していますが、後々は自分でイラレを使って描く予定です キャラクターはイラレを使って自分で書いているものです

---
+++ numberDetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  numberDetailViewController.swift
+//  syuwaapp
+//
+//  Created by 中井日向子 on 2021/03/24.
+//
+
+import UIKit
+
+class numberDetailViewController: UIViewController {
+    let numberArray: [String] = ["1","2","3","4","5","6","7","8","9","0",]
+    let images : [String] = ["iti.png","nii.png","san.png","yonn.png","go.png","roku.png","nana.png","hati.png","kyu.png","zero.png",]
+    let details :[String] =
+        ["人差し指を立てる","１の手に中指を加える","２の手に薬指を加える","３の手に小指を加える","親指を横に伸ばし、他の四本の指を握る","手の甲を相手に向けて、親指と人差し指でL字を作る","６の手に中指を加える","７の手に薬指を加える","８の手に小指を加える","小指が相手に見えるようにして丸めた手が「０」に見えるようにする",]
+    
+    var index = 0
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var imageView2: UIImageView!
+    @IBOutlet var Label: UILabel!
+    @IBOutlet var characterLabel: UILabel!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- syuwaapp/characterListViewController.swift
+++ syuwaapp/characterListViewController.swift
@@ -8,16 +8,121 @@
 import UIKit
 
 class characterListViewController: UIViewController {
-
+    var index = 0
+    @IBOutlet var aButton: UIButton!
+    @IBOutlet var iButton: UIButton!
+    @IBOutlet var uButton: UIButton!
+    @IBOutlet var eButton: UIButton!
+    @IBOutlet var oButton: UIButton!
+    @IBOutlet var kaButton: UIButton!
+    @IBOutlet var kiButton: UIButton!
+    @IBOutlet var kuButton: UIButton!
+    @IBOutlet var keButton: UIButton!
+    @IBOutlet var koButton: UIButton!
+    @IBOutlet var saButton: UIButton!
+    @IBOutlet var siButton: UIButton!
+    @IBOutlet var suButton: UIButton!
+    @IBOutlet var seButton: UIButton!
+    @IBOutlet var soButton: UIButton!
+    @IBOutlet var taButton: UIButton!
+    @IBOutlet var tiButton: UIButton!
+    @IBOutlet var tuButton: UIButton!
+    @IBOutlet var teButton: UIButton!
+    @IBOutlet var toButton: UIButton!
+    @IBOutlet var naButton: UIButton!
+    @IBOutlet var niButton: UIButton!
+    @IBOutlet var nuButton: UIButton!
+    @IBOutlet var neButton: UIButton!
+    @IBOutlet var noButton: UIButton!
+    @IBOutlet var haButton: UIButton!
+    @IBOutlet var hiButton: UIButton!
+    @IBOutlet var fuButton: UIButton!
+    @IBOutlet var heButton: UIButton!
+    @IBOutlet var hoButton: UIButton!
+    @IBOutlet var maButton: UIButton!
+    @IBOutlet var miButton: UIButton!
+    @IBOutlet var muButton: UIButton!
+    @IBOutlet var meButton: UIButton!
+    @IBOutlet var moButton: UIButton!
+    @IBOutlet var yaButton: UIButton!
+    @IBOutlet var yuButton: UIButton!
+    @IBOutlet var yoButton: UIButton!
+    @IBOutlet var raButton: UIButton!
+    @IBOutlet var riButton: UIButton!
+    @IBOutlet var ruButton: UIButton!
+    @IBOutlet var reButton: UIButton!
+    @IBOutlet var roButton: UIButton!
+    @IBOutlet var waButton: UIButton!
+    @IBOutlet var woButton: UIButton!
+    @IBOutlet var nnButton: UIButton!
+    
+    let cornerRadius: CGFloat = 30.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        aButton.layer.cornerRadius = cornerRadius
+        iButton.layer.cornerRadius = cornerRadius
+        uButton.layer.cornerRadius = cornerRadius
+        eButton.layer.cornerRadius = cornerRadius
+        oButton.layer.cornerRadius = cornerRadius
+        kaButton.layer.cornerRadius = cornerRadius
+        kiButton.layer.cornerRadius = cornerRadius
+        kuButton.layer.cornerRadius = cornerRadius
+        keButton.layer.cornerRadius = cornerRadius
+        koButton.layer.cornerRadius = cornerRadius
+        saButton.layer.cornerRadius = cornerRadius
+        siButton.layer.cornerRadius = cornerRadius
+        suButton.layer.cornerRadius = cornerRadius
+        seButton.layer.cornerRadius = cornerRadius
+        soButton.layer.cornerRadius = cornerRadius
+        taButton.layer.cornerRadius = cornerRadius
+        tiButton.layer.cornerRadius = cornerRadius
+        tuButton.layer.cornerRadius = cornerRadius
+        teButton.layer.cornerRadius = cornerRadius
+        toButton.layer.cornerRadius = cornerRadius
+        naButton.layer.cornerRadius = cornerRadius
+        niButton.layer.cornerRadius = cornerRadius
+        nuButton.layer.cornerRadius = cornerRadius
+        neButton.layer.cornerRadius = cornerRadius
+        noButton.layer.cornerRadius = cornerRadius
+        haButton.layer.cornerRadius = cornerRadius
+        hiButton.layer.cornerRadius = cornerRadius
+        fuButton.layer.cornerRadius = cornerRadius
+        heButton.layer.cornerRadius = cornerRadius
+        hoButton.layer.cornerRadius = cornerRadius
+        maButton.layer.cornerRadius = cornerRadius
+        miButton.layer.cornerRadius = cornerRadius
+        muButton.layer.cornerRadius = cornerRadius
+        meButton.layer.cornerRadius = cornerRadius
+        moButton.layer.cornerRadius = cornerRadius
+        yaButton.layer.cornerRadius = cornerRadius
+        yuButton.layer.cornerRadius = cornerRadius
+        yoButton.layer.cornerRadius = cornerRadius
+        raButton.layer.cornerRadius = cornerRadius
+        riButton.layer.cornerRadius = cornerRadius
+        ruButton.layer.cornerRadius = cornerRadius
+        reButton.layer.cornerRadius = cornerRadius
+        roButton.layer.cornerRadius = cornerRadius
+        waButton.layer.cornerRadius = cornerRadius
+        woButton.layer.cornerRadius = cornerRadius
+        nnButton.layer.cornerRadius = cornerRadius
+        
+        
     }
     
-    @IBAction func toDetail(sender :Any){
+    @IBAction func toDetail(sender :UIButton){
+        index = sender.tag
+        performSegue(withIdentifier: "tochallenge", sender: self)
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination as! characterDetailViewController
+        destination.index = self.index
     }
+
     /*
     // MARK: - Navigation
 
